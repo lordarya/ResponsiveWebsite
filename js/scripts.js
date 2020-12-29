e4a45a92549b6fdfdf766cbd0c1d54d3ef1d9ab8
lordaryam <12aryacandrayana@gmail.com>
2020-12-29 20:49:10 +0800

    replace project

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,31 @@
-const nav = document.querySelector('nav ul');
+// const nav = document.querySelector('.menu-list ul');
+// const menuToggle = ducument.querySelector('.menu-toggle input');
+
+// menuToggle.addEventListener('checked', function() {
+//     nav.classList.toggle('slide');
+// });
+
+// Sticky Navbar
+
+window.onscroll = function(){
+    stickyNav();
+}
+
+const nav = document.querySelector('nav');
+
+const sticky = nav.offsetTop;
+
+function stickyNav() {
+    if (window.scrollY >= 300){
+        nav.classList.add('sticky');
+    }else {
+        nav.classList.remove('sticky');
+    }
+}
+
+
+const navbar = document.querySelector('.menu-list ul');
 
 function toggle() {
-  nav.classList.toggle('slide');    
+  navbar.classList.toggle('slide');    
  }
